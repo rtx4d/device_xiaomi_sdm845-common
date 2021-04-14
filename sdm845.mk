eeb8c186f5215a79625a4e5d8272ddd363473e5f
Andrey Sprynga <rtx4d@aospa.co>
2021-04-14 22:08:54 +0700

    sdm845-common: drop wfd
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- sdm845.mk
+++ sdm845.mk
@@ -301,10 +301,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-PRODUCT_PACKAGES += \
-    WfdCommon \
-    WfdService
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
