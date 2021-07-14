2d3dd619b17958c7f9f8f2ab31044e975c66ca01
Andrey Sprynga <rtx4d@aospa.co>
2021-07-14 10:16:15 +0700

    sdm845-common: Update Bluetooth blobs
    * OnePlus/OnePlus6/OnePlus6:11/RKQ1.201217.002/2106301530:user/release-keys
    
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- sdm845.mk
+++ sdm845.mk
@@ -91,9 +91,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
