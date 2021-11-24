a8d9152f92b9f91a4f6c20d48984ba74b5c7e141
Andrey Sprynga <rtx4d@aospa.co>
2021-11-24 16:03:56 +0700

    sdm845-common: move IFAA in another repo
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- sdm845.mk
+++ sdm845.mk
@@ -139,11 +139,7 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+include vendor/xiaomi-ifaa/ifaa.mk
 
 # Input
 PRODUCT_COPY_FILES += \
