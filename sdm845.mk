2e14d765d9a7c2bc62257647147f60dda008cd5c
Arvind Kumar <karvin@codeaurora.org>
2021-12-29 15:27:07 +0700

    sdm845-common: Define SOC properties
     * Set soc model and manufacturer properties

--- sdm845.mk
+++ sdm845.mk
@@ -234,6 +234,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# SOC Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI \
+    ro.soc.model=SDM845
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
