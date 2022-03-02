6cdd9c0bf15e6ccb5dd729687ed287707a9f26a5
MrArtemSid <>
2022-03-02 13:05:36 +0700

    sdm845-common: Build sound_trigger.primary.sdm845 from source
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>
    Change-Id: I0c134cf4af1c57860711c5bf7522fb9057a03d4f

--- sdm845.mk
+++ sdm845.mk
@@ -78,7 +78,8 @@ PRODUCT_PACKAGES += \
     libspkrprot \
     libtinycompress \
     libvolumelistener \
-    tinymix
+    tinymix \
+    sound_trigger.primary.sdm845
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
